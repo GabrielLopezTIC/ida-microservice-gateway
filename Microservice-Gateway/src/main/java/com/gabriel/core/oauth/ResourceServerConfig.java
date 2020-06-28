24e60316b3e244a88e9d8643f3d0b244901cf681
Gabriel López <gabriellopeztic@gmail.com>
2020-06-27 23:35:23 -0500

    primer commit

---
+++ Microservice-Gateway/src/main/java/com/gabriel/core/oauth/ResourceServerConfig.java
@@ -0,0 +1,51 @@
+ package com.gabriel.core.oauth;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore());
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.cors()
+		.and()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/api/users/users/find/all").hasRole("ADMIN")
+		.antMatchers(HttpMethod.POST,"/api/security/oauth/token").permitAll()
+		.antMatchers(HttpMethod.GET,"/api/users/users/find/usr/{email}").permitAll()
+		.antMatchers(HttpMethod.DELETE,"/api/users/users/delete/usr/{email}").permitAll()
+		.antMatchers(HttpMethod.POST,"/api/users/users/save/usr").permitAll()
+		.antMatchers(HttpMethod.POST,"/api/ida/file/uploadFile").hasRole("USER")
+		.anyRequest().authenticated();
+		
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accesTokenConverter());
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accesTokenConverter() {
+		JwtAccessTokenConverter token = new JwtAccessTokenConverter();
+		token.setSigningKey("claveToken");
+		return token;
+	}
+	
+
+}
